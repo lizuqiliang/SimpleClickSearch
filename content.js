bc710b7f6c9a5f2d4af10ba6e34e0b0f593826bc
lizuqiliang <lizuqiliang@gmail.com>
2014-08-27 19:28:14 -0400

    Added Google Extension Project: Perform a search by  clicking a word/highlight a group of text whilst holding a set key. Hold G to perform Google Search. Hold W to perform Wikipedia Search. Hold S to perform Stack Overflow Search.

---
+++ content.js
@@ -0,0 +1,81 @@
+var googleKey = 71;	//default to g
+var SOKey = 83;		//edfault to x
+var WikiKey = 87;	//default to c
+
+var googleSelect = false;
+var SOSelect = false;
+var WikiSelect = false;
+
+$("body").click(function(e) {
+	if (googleSelect) {
+		googleSelect = false;
+		go2Google(getSelectedText());
+	} else if (SOSelect) {
+		SOSelect = false;
+		go2SO(getSelectedText());
+	} else if (WikiSelect) {
+		WikiSelect = false;
+		go2Wiki(getSelectedText());
+	}
+});
+
+function getSelectedText() {
+	var selectedText = '';
+	var selection = window.getSelection();
+	if (selection.isCollapsed) {
+	    selection.modify('move', 'forward', 'character');
+	    selection.modify('move', 'backward', 'word');
+	    selection.modify('extend', 'forward', 'word');
+	    selectedText = selection.toString();
+	    selection.modify('move', 'forward', 'character');
+	}
+	else {
+	    selectedText = selection.toString();
+	}
+	return selectedText;
+}
+
+$(window).keydown(function(e) {
+	if (e.which == googleKey) {
+		googleSelect = true;
+	} else if (e.which == SOKey) {
+		SOSelect = true;
+	} else if (e.which == WikiKey) {
+		WikiSelect = true;
+	}
+}).keyup(function(e) {
+	if (e.which == googleKey) { 
+		googleSelect = false;
+	} else if (e.which == SOKey) {
+		SOSelect = false;
+	} else if (e.which == WikiKey) {
+		WikiSelect = false;
+	}
+});
+
+function go2Google(searchItem) {
+	var words = searchItem.split(' ');
+	searchURL = "http://www.google.ca/search?q=";
+	for(var i=0;i<words.length;i++) {
+		searchURL+=words[i]+"+";
+	}
+	window.open(searchURL);
+}
+
+function go2SO(searchItem) {
+	var words = searchItem.split(' ');
+	searchURL = "http://stackoverflow.com/search?q=";
+	for(var i=0;i<words.length;i++) {
+		searchURL+=words[i]+"+";
+	}
+	window.open(searchURL);
+}
+
+function go2Wiki(searchItem) {
+	var words = searchItem.split(' ');
+	searchURL = "http://en.wikipedia.org/w/index.php?search=";
+	for(var i=0;i<words.length;i++) {
+		searchURL+=words[i]+"+";
+	}
+	window.open(searchURL);
+}
