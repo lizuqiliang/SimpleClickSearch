8fe6ea9467e07b7c744c5690b33976e8513b7c6d
Zuqi Li <lizuqiliang@gmail.com>
2015-04-18 23:42:07 -0400

    Improved browser action UI

--- browseraction/popup.js
+++ browseraction/popup.js
@@ -1,41 +1,25 @@
-var lists = [
-	"ABCDEFG",
-	"HIJKLMN",
-	"OPQRST",
-	"UVWXYZ"];
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 $(document).ready(function() {
 	AtoZMenuGenerator();
 });
 
 function AtoZMenuGenerator() {
-	AtoZListGenerator(0,"A~G");
-	AtoZListGenerator(1,"H~N");
-	AtoZListGenerator(2,"O~T");
-	AtoZListGenerator(3,"U~Z");
-	for (var i = 0; i < lists.length; i++) { 
-        AtoZItemsGenerator(i.toString(), lists[i]);
-    };
+	AtoZItemsGenerator()
 }
 
-function AtoZListGenerator(num, text) {
-	var str = "<li>"
-	$(".createAtoZ").append(str.concat(text).concat("<ul class='list").concat(num.toString()).concat("'></ul></li>"));
-}
-
-function AtoZItemsGenerator(num, list) {
-	var selector = ".list";
-	selector=selector.concat(num);
+function AtoZItemsGenerator() {
+	var selector = ".createAtoZ";
     var li = "<li class='click'>";
-	for (var i = 0; i < list.length; i++) {
-		$(selector).append(li.concat(list.charAt(i)).concat("</li>"));
+	for (var i = 0; i < alphabet.length; i++) {
+		$(selector).append(li.concat(alphabet.charAt(i)).concat("</li>"));
     };
 }
 
 $(function(){
 	$( ".click" ).click(function() {
 		var key = $(this).text();
-		var listStr=$(this).parent().parent().parent().parent().text();
+		var listStr=$(this).parent().parent().text();
 		var searchEngine = searchEngineFilter(listStr);
 		saveChanges(searchEngine, key);
 		$(".output").text("\"" + key + "\" key set for " + searchEngine + "!");
