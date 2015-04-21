6d50654d57d4d315aff8c02a4978e391ebd54b09
Zuqi Li <lizuqiliang@gmail.com>
2015-04-21 01:00:56 -0400

    Improved browser action UI

--- browseraction/popup.js
+++ browseraction/popup.js
@@ -1,41 +1,52 @@
+var searchEngineList = [
+	"Google",
+	"Youtube",
+	"Wikipedia",
+	"Stack Overflow",
+	"Baidu",
+	"Bing",
+	"Yahoo"
+];
+
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 $(document).ready(function() {
-	AtoZMenuGenerator();
+	makeMenus();
 });
 
-function AtoZMenuGenerator() {
-	AtoZItemsGenerator()
+function menuAlphabetBuilder() {
+	var str = "";
+	for (var i = 0, len = alphabet.length; i < len; i++) {
+		str += "<li><a class=\"click\" href=\"#\">" + alphabet[i] + "</a></li>";
+	}
+	return str;
 }
 
-function AtoZItemsGenerator() {
-	var selector = ".createAtoZ";
-    var li = "<li class='click'>";
-	for (var i = 0; i < alphabet.length; i++) {
-		$(selector).append(li.concat(alphabet.charAt(i)).concat("</li>"));
-    };
+function menuStringBuilder(searchEngine) {
+	return "<div class=\"dropdown custom-dropdown\"><button class=\"btn btn-default dropdown-toggle custom-button\" type=\"button\" data-toggle=\"dropdown\">" + 
+		searchEngine +
+		"<span class=\"caret\"></span></button><ul class=\"dropdown-menu scrollable-menu\" value=\"" +
+		searchEngine + 
+		"\" aria-labelledby=\"" +
+		searchEngine +
+		"\">" + menuAlphabetBuilder();
+}
+
+function makeMenus() {
+	for (var i = 0; i < searchEngineList.length; i++) {
+		$(".menus").append(menuStringBuilder(searchEngineList[i]));
+    }
 }
 
 $(function(){
 	$( ".click" ).click(function() {
 		var key = $(this).text();
-		var listStr=$(this).parent().parent().text();
-		var searchEngine = searchEngineFilter(listStr);
+		var searchEngine=$(this).parent().parent().attr("value");
 		saveChanges(searchEngine, key);
 		$(".output").text("\"" + key + "\" key set for " + searchEngine + "!");
 		$(".notice").text("Please refresh browser!");
 	});
 });
-
-function searchEngineFilter(str) {
-    var i;
-    for(i = 0; i < str.length; i++) {
-        if(str[i] == '\n') {
-            break;
-        }
-    }
-    return str.substring(0,i);
-}
 
 function saveChanges(searchEngine, key) {
 	var setting= {};
